4c8ff8a733f2bef1f520442abe3542c069a8bd0a
Taylan <taylancankose@gmail.com>
2024-05-27 23:29:12 +0300

    week project completed

---
+++ src/main/java/org/example/Company.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Company {
+    private int id;
+    private String name;
+    private double giro;
+    private String[] developerNames;
+
+    public Company(int id, String name, double giro, String[] developerNames) {
+        this.id = id;
+        this.name = name;
+        setGiro(giro);
+        this.developerNames = developerNames;
+    }
+
+    public void addEmployee(int index, String name) {
+        if (index < 0 || index >= developerNames.length) {
+            System.out.println("Invalid index: " + index);
+        } else if (developerNames[index] != null) {
+            System.out.println("Developer at index " + index + " is already occupied.");
+        } else {
+            developerNames[index] = name;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGiro() {
+        return giro;
+    }
+
+    public void setGiro(double giro) {
+        if (giro < 0) {
+            throw new IllegalArgumentException("Giro cannot be negative.");
+        }
+        this.giro = giro;
+    }
+
+    public String[] getDeveloperNames() {
+        return developerNames;
+    }
+
+    public void setDeveloperNames(String[] developerNames) {
+        this.developerNames = developerNames;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", giro=" + giro +
+                ", developerNames=" + Arrays.toString(developerNames) +
+                '}';
+    }
+}

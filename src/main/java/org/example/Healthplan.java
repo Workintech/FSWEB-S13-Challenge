2bd45e9ebcddf5df9998be339096a20b45031e85
ridvanbozoglu <ridvanbozoglu.5@gmail.com>
2024-09-20 14:32:59 +0300

    task is done.

---
+++ src/main/java/org/example/Healthplan.java
@@ -0,0 +1,47 @@
+package org.example;
+import org.example.enums.Plan;
+
+public class Healthplan {
+    private Integer id;
+    private String name;
+    private Plan plan;
+
+    @Override
+    public String toString() {
+        return "Healthplan{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", plan='" + plan + '\'' +
+                '}';
+    }
+
+    public Healthplan(Integer id, String name, Plan plan) {
+        this.id = id;
+        this.name = name;
+        this.plan = plan;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Plan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(Plan plan) {
+        this.plan = plan;
+    }
+}
